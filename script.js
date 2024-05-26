a5e6cbd006a2e04ec1abd503775a738066229d97
iyioladebami <148291606+iyioladebami@users.noreply.github.com>
2024-05-26 16:51:49 +0100

    created

---
+++ script.js
@@ -0,0 +1,16 @@
+let section = document.getElementById("section");
+function appear(params) {
+    let shareIcons = document.getElementById("shareicons");
+    shareIcons.style.opacity="1";
+    shareIcons.style.display="block";
+    let shareIcon = document.getElementById("shareicon");
+    shareIcon.onclick = normal;
+}
+function normal(){
+    let shareIcons = document.getElementById("shareicons");
+    shareIcons.style.opacity="0";
+    shareIcons.style.display="hidden";
+    shareIcons.style.transition="1s";
+    let shareIcon = document.getElementById("shareicon");
+    shareIcon.onclick = appear;
+}
